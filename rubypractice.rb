8d6cd29810452c139e5c806c76b22defbaefaf2a
Brittany Russell <sincerelybr@gmail.com>
2020-02-24 15:44:52 +0000

    Done.

---
+++ rubypractice.rb
@@ -0,0 +1,28 @@
+current_president = "Barack \n Obama"
+puts "In 2016, the \"president\" was #{current_president}."
+
+first_number = 7
+second_number = 14
+ 
+sum = first_number + second_number
+ 
+puts sum
+
+puts "programming".upcase()
+
+puts 2+3
+puts 2*3
+puts 2/3
+puts 2-3
+puts 2**3 #2^3
+puts 2%3
+
+num = -20.62
+puts ("my fav number is" + num.to_s) #changes num from number to str
+
+puts num.abs() #absolute value
+puts num.round() #rounded number
+puts num.ceil()
+puts num.floor()
+puts Math.sqrt(36)
+
